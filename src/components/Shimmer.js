c9cfb9dca62da488873aaaddffba86fecad1ea25
Kautilya-23 <kautilyasathwara@gmail.com>
2025-12-31 11:32:33 +0530

    Fixing the small bugs

---
+++ src/components/Shimmer.js
@@ -0,0 +1,20 @@
+const Shimmer = () =>{
+    return(
+        <div className="shimmer flex-wrap flex">
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+            <div className="shimmer-card w-[240px] h-[380px] bg-slate-100 m-4"></div>
+        </div>
+    )
+}
+
+export default Shimmer;
